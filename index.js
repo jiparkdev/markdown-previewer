6130e368367b37ccecfa9b676a8350f09fa30180
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-05-29 23:40:21 -0700

    Add initial files.

---
+++ index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  getNewQuote() {}
+
+  render() {}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
